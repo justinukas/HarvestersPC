0f4c3e3e96fe1c0362a051c0c202e96771f2690a
justinukas <165898137+justinukas@users.noreply.github.com>
2024-07-23 18:42:23 +0300

    simplified item interactions script a bit

--- Assets/Scripts/Controls/Item Interactions.cs
+++ Assets/Scripts/Controls/Item Interactions.cs
@@ -8,11 +8,8 @@ public class ItemInteractions : MonoBehaviour
     private string currentItem = "null";
     private GameObject grabbedObject; // gameobject that the raycast hit
 
-    private Transform camTransform;
-    private void Start()
-    {
-        camTransform = Camera.main.transform;
-    }
+    // transform for default tool position
+    public Transform defaultToolPosition;
 
     private void Update()
     {
@@ -22,25 +19,22 @@ public class ItemInteractions : MonoBehaviour
         ItemSwing();
     }
 
-    // raycast stuff
-    private Ray ray;
-    private RaycastHit hit;
-
     // checks for raycast collision within 1.5 units and in front of camera on the press of E
     private void ItemInteractionCheck()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-            ray = new Ray(camTransform.position, camTransform.forward);
-            if (Physics.Raycast(ray, out hit, 1.5f, 1 << 6 /* takes 6th (grabbables) layermask */) && currentItem == "null")
+            Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+            if (Physics.Raycast(ray, out RaycastHit hit, 1.5f, 1 << 6 /* takes 6th (grabbables) layermask */) && currentItem == "null")
             {
                 grabbedObject = hit.collider.gameObject;
                 string itemName = grabbedObject.name;
 
                 if (itemName == "Scythe" || itemName == "Axe" || itemName == "Hoe")
                 {
-                    grabbedObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePosition; // lock rigidbody from moving
                     currentItem = itemName;
+
+                    grabbedObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePosition; // lock rigidbody from moving
                 }
             }
         }
@@ -52,11 +46,12 @@ public class ItemInteractions : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.R) && currentItem != "null")
         {
             grabbedObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+            grabbedObject.transform.Find(currentItem).GetComponent<Animator>().Play("DefaultState");
+            isSwinging = false;
             currentItem = "null";
             grabbedObject = null;
         }
     }
-
 
     private void ItemPositionAndRotation()
     {
@@ -68,29 +63,29 @@ public class ItemInteractions : MonoBehaviour
         switch (currentItem)
         {
             case "Scythe":
-                offset = new Vector3(0, -0.25f, 0);
+                offset = new Vector3(0, 0, 0);
                 break;
             case "Axe":
-                offset = new Vector3(0, -0.15f, 0);
+                offset = new Vector3(0, 0, 0);
                 break;
             case "Hoe":
-                offset = new Vector3(0, -0.2f, 0);
+                offset = new Vector3(0, 0, 0);
                 break;
         }
-        grabbedObject.transform.position = camTransform.position + offset + camTransform.forward * 0.3f;
-
+        defaultToolPosition.position = defaultToolPosition.position + offset;
+        grabbedObject.transform.position = defaultToolPosition.position;
 
         // set rotations
         switch (currentItem)
         {
             case "Scythe":
-                grabbedObject.gameObject.transform.rotation = Quaternion.LookRotation(camTransform.forward * 0.8f, camTransform.up * 0.8f) * Quaternion.Euler(-90, 180, 90);
+                grabbedObject.gameObject.transform.rotation = defaultToolPosition.rotation * Quaternion.Euler(-90, 180, 90);
                 break;
             case "Axe":
-                grabbedObject.gameObject.transform.rotation = Quaternion.LookRotation(camTransform.forward * 0.8f, camTransform.up * 0.8f);
+                grabbedObject.gameObject.transform.rotation = defaultToolPosition.rotation;
                 break;
             case "Hoe":
-                grabbedObject.gameObject.transform.rotation = Quaternion.LookRotation(camTransform.forward * 0.8f, camTransform.up * 0.8f) * Quaternion.Euler(0, -90, 0);
+                grabbedObject.gameObject.transform.rotation = defaultToolPosition.rotation * Quaternion.Euler(0, -90, 0);
                 break;
         }
     }
@@ -103,12 +98,23 @@ public class ItemInteractions : MonoBehaviour
         Animator currentAnimator = grabbedObject.transform.Find(currentItem).GetComponent<Animator>();
         if (Input.GetMouseButtonDown(0))
         {
-            if (currentAnimator.GetCurrentAnimatorStateInfo(0).IsName($"{currentItem} Swing") == false)
+            if (currentItem == "Scythe" || currentItem == "Axe" || currentItem == "Hoe")
             {
-                currentAnimator.Play($"{currentItem} Swing");
+                if (currentAnimator.GetCurrentAnimatorStateInfo(0).IsName($"{currentItem} Swing") == false)
+                {
+                    currentAnimator.Play($"{currentItem} Swing");
+                }
+            }
+            if (currentItem == "Bag")
+            {
+                // do stuff
             }
         }
 
-        isSwinging = currentAnimator.GetCurrentAnimatorStateInfo(0).IsName($"{currentItem} Swing");
+        if (currentItem == "Scythe" || currentItem == "Axe" || currentItem == "Hoe")
+        { isSwinging = currentAnimator.GetCurrentAnimatorStateInfo(0).IsName($"{currentItem} Swing"); }
+
+        if (currentItem == "Bag")
+        { /*play anim */}
     }
 }
