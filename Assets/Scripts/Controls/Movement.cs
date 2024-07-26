984b11a3cf358cf69ea28f3765ceb0bc3f3cdf07
justinukas <165898137+justinukas@users.noreply.github.com>
2024-07-26 22:30:53 +0300

    added jumping; rewrote "Movement"; sorted assets

--- Assets/Scripts/Controls/Movement.cs
+++ Assets/Scripts/Controls/Movement.cs
@@ -2,68 +2,43 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    private readonly float gravity = -9.81f;
+
+    private Vector3 velocity;
     private int playerSpeed = 2;
-
-    private float x;
-    private float z;
-
-    private float gravity;
-
-    private float jump;
-    private float jumpForce;
-    private float jumpDuration;
-
+    private float jumpHeight = 0.5f;
     private bool isJumping = false;
-
-    private Vector3 move;
-
     private bool isGrounded;
 
     void Update()
     {
-        Debug.Log(isJumping);
-
-
         isGrounded = GetComponent<CharacterController>().isGrounded;
-
-        gravity -= 9.81f * Time.deltaTime;
-        x = Input.GetAxisRaw("Horizontal");
-        z = Input.GetAxisRaw("Vertical");
-        if (isJumping == false)
+        if (isGrounded && velocity.y < 0f)
         {
-            move = transform.right * x * playerSpeed * Time.deltaTime + transform.forward * z * playerSpeed * Time.deltaTime + transform.up * gravity;
+            velocity.y = 0f;
         }
 
-        if (isJumping == true)
-        {
-            move = transform.right * x * playerSpeed * Time.deltaTime + transform.forward * z * playerSpeed * Time.deltaTime + transform.up * jumpForce;
-        }
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+        Vector3 move = transform.right * x + transform.forward * z;
         
-        GetComponent<CharacterController>().Move(move);
-        if (isGrounded)
-        {
-            gravity = 0;
-        }
 
-        if (isJumping == true && isGrounded)
+        if (isJumping == true /*Input.GetKeyDown(KeyCode.Space)*/ && isGrounded)
         {
-            gravity = 0;
-            //jumpForce += 1f * Time.deltaTime;
-            jump += 10 * Time.deltaTime;
-            jumpDuration += Time.deltaTime;
-
-        }
-        if (jumpDuration > 0.5f)
-        {
+            velocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravity);
             isJumping = false;
-            jumpDuration = 0f;
-            jumpForce = 0f;
-            jump = 0f;
         }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        GetComponent<CharacterController>().Move(velocity * Time.deltaTime + move * Time.deltaTime * playerSpeed);
     }
 
     public void Jump()
     {
-        isJumping = true;
+        if (isJumping != true)
+        {
+            isJumping = true;
+        }
     }
 }
