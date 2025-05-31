9ad6f1747a362219289d62fc753393e3f4a468a7
justinukas <165898137+justinukas@users.noreply.github.com>
2025-05-31 15:11:23 +0300

    refactoring inventory to use SO's cause i just remembered i had those :P

---
+++ Assets/HarvestersPC assets/Scripts/Item Handling/kid named inv/Item.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Scriptable Objects/Inventory/New Item")]
+public class Item : ScriptableObject
+{
+    public int ID;
+    public string itemName;
+    public GameObject itemObject;
+    public GameObject itemSprite;
+    public ItemType itemType;
+}
+
+public enum ItemType
+{
+    Tool,
+    Seedbag,
+    Crop
+}
